3ef3f754c465af2eb143f0c22059feb9be6c4c69
youseokhwan <tjrghks115@naver.com>
2020-01-29 03:00:31 +0900

    Django: django_cors_headers 설치

--- backend/todolist/settings.py
+++ backend/todolist/settings.py
@@ -27,9 +27,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'webpack_loader',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -38,6 +40,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/',
+)
 
 ROOT_URLCONF = 'todolist.urls'
 
